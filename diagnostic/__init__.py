72076f2276a8a1decbfbb15c00f2b25b383a9e4e
Promptops <dragonkoa1490@gmail.com>
2026-09-08 22:40:38 -0400

    Expose diagnostic engine API

---
+++ diagnostic/__init__.py
@@ -0,0 +1,4 @@
+from .engine import evaluate
+from .model import AssessmentInput, AssessmentResult, EvidenceConfidence
+
+__all__ = ["evaluate", "AssessmentInput", "AssessmentResult", "EvidenceConfidence"]
